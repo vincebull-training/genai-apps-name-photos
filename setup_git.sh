74805695f235fd93de73db2688ad942561427291
vince-bull <vincebull@hotmail.fr>
2026-04-03 22:08:47 +0200

    Update setup_git.sh

--- setup_git.sh
+++ setup_git.sh
@@ -2,3 +2,8 @@
 git config user.email "vincebull@hotmail.fr"
 git config user.name "vince-bull"
 echo "Configuration Git appliquée localement !"
+pip3 install virtualenv 
+virtualenv my_env # create a virtual environment my_env
+source my_env/bin/activate # activate my_env
+# installing required libraries in my_env
+pip install langchain==0.1.11 gradio==5.23.2 transformers==4.38.2 bs4==0.0.2 requests==2.31.0 torch==2.2.1
